1f0c93205057b6ecb44d9336d89e31100d27cc15
amirdr81 <amirmohammadd@yahoo.com>
2022-02-07 10:47:15 +0330

    CMakeLists.txt & SDL2

---
+++ build/CMakeFiles/state.io.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/state.io.dir/src/main.c.o"
+  "CMakeFiles/state.io.dir/src/main.c.o.d"
+  "state.io"
+  "state.io.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/state.io.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,17 @@
+//main libraries
+#include <stdio.h>
+
+//new & external libraries
+#include "SDL.h"
+#include "SDL2_gfxPrimitives.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+
+//functions
+
+
+//main
+int main()
+{
+
+}
